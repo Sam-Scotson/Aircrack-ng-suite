3eea49c915b10dd4e34789ebdeb1f35ecacec712
Sam Scotson <118854225+Sam-Scotson@users.noreply.github.com>
2023-06-08 22:59:43 +1200

    Update airbase-ng.py

--- Aircrack-ng Suite/airbase-ng.py
+++ Aircrack-ng Suite/airbase-ng.py
@@ -1,7 +1,7 @@
 import subprocess
 from boltdb import BoltDB
 
-def execute_airbase_ng():
+def exe_airbase_ng():
     try:
         output = subprocess.check_output(['airbase-ng', '-v'])
         result = output.decode('utf-8')
@@ -11,9 +11,9 @@ def execute_airbase_ng():
         db.insert("Airbase-ng-results", result)
         db.commit()
         db.close()
-        print("Airbase-ng results saved to BoltDB successfully.")
+        print("Airbase results saved successfully.")
     except Exception as e:
         print("Error executing airbase-ng or saving results:", str(e))
 
 if __name__ == "__main__":
-    execute_airbase_ng()
+    exe_airbase_ng()
